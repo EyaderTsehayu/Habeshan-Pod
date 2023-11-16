3469f0f6be00af40fc0f1c0e3e198439bd70541d
Eyader Tsehayu <eyaderts@gmail.com>
2023-11-17 00:15:28 +0300

    implement welcome component on home

---
+++ components/home/welcome/Welcome.tsx
@@ -0,0 +1,52 @@
+import { View, Text, TextInput, TouchableOpacity, Image } from "react-native";
+import welcomeStyles from "./welcome.style";
+import React from "react";
+import { Ionicons } from "@expo/vector-icons";
+import Colors from "@/constants/Colors";
+//import podcast from "@/assets/images/podcast.png";
+
+interface WelcomeProps {
+  setSearchTerm: React.Dispatch<React.SetStateAction<string>>;
+  searchTerm: string;
+  handleClick: () => void;
+}
+
+const Welcome: React.FC<WelcomeProps> = ({
+  setSearchTerm,
+  searchTerm,
+  handleClick,
+}) => {
+  return (
+    <View style={welcomeStyles.container}>
+      <View style={welcomeStyles.searchContainer}>
+        <View style={welcomeStyles.searchWrapper}>
+          <TextInput
+            style={welcomeStyles.searchInput}
+            value={searchTerm}
+            onChangeText={(text) => setSearchTerm(text)}
+            placeholder="What are you looking for?"
+          />
+        </View>
+        <TouchableOpacity style={welcomeStyles.searchBtn} onPress={handleClick}>
+          <Ionicons name="search" size={24} color={Colors.primaryText} />
+        </TouchableOpacity>
+      </View>
+      <View style={welcomeStyles.hero}>
+        <View style={welcomeStyles.welcomeMsgContainer}>
+          <Text style={welcomeStyles.welcomeMsg}>
+            Discover your favorite{" "}
+            <Text style={{ fontFamily: "dm-b", fontSize: 40 }}>podcast</Text>
+          </Text>
+        </View>
+        <View>
+          <Image
+            source={require("../../../assets/images/podcast.png")}
+            style={welcomeStyles.welcomeImg}
+          />
+        </View>
+      </View>
+    </View>
+  );
+};
+
+export default Welcome;
---
+++ components/home/welcome/welcome.style.js
@@ -0,0 +1,65 @@
+import { StyleSheet } from "react-native";
+import Colors from "@/constants/Colors";
+
+const welcomeStyles = StyleSheet.create({
+  container: {
+    width: "100%",
+    flexDirection: "column",
+    // borderBottomLeftRadius: 30,
+    paddingHorizontal: 12,
+  },
+  searchContainer: {
+    justifyContent: "center",
+    alignItems: "center",
+    flexDirection: "row",
+    marginTop: 20,
+    height: 40,
+  },
+  searchWrapper: {
+    flex: 1,
+    backgroundColor: Colors.secondary,
+    color: Colors.primaryText,
+    marginRight: 10,
+    justifyContent: "center",
+    alignItems: "center",
+    borderRadius: 10,
+    height: "100%",
+  },
+  searchInput: {
+    fontFamily: "dm",
+    width: "100%",
+    height: "100%",
+    paddingHorizontal: 25,
+    color: Colors.primaryText,
+  },
+  searchBtn: {
+    width: 50,
+    height: "100%",
+    backgroundColor: Colors.secondary,
+    borderRadius: 25,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+
+  hero: {
+    display: "flex",
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "space-between",
+    width: "100%",
+    paddingTop: 5,
+  },
+  welcomeMsgContainer: { width: "49%" },
+  welcomeMsg: {
+    color: Colors.primaryText,
+    fontFamily: "dm-sb",
+    fontSize: 32,
+  },
+  welcomeImg: {
+    height: 200,
+    width: 200,
+    resizeMode: "contain",
+  },
+});
+
+export default welcomeStyles;
