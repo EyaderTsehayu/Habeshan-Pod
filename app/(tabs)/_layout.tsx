021cb70f41be6ce5effbecef6810997c21cc1797
Eyader Tsehayu <eyaderts@gmail.com>
2023-11-24 20:13:53 +0300

    initial tab pages

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,12 +1,16 @@
 import { Tabs } from "expo-router";
 import { Ionicons } from "@expo/vector-icons";
 import Colors from "@/constants/Colors";
+import { MaterialIcons } from "@expo/vector-icons";
+import { MaterialCommunityIcons } from "@expo/vector-icons";
+import { Entypo } from "@expo/vector-icons";
 
 const Layout = () => {
   return (
     <Tabs
       screenOptions={{
         tabBarActiveTintColor: Colors.primary,
+        tabBarInactiveTintColor: Colors.headerText,
         tabBarLabelStyle: {
           fontFamily: "dm-sb",
         },
@@ -17,11 +21,44 @@ const Layout = () => {
         options={{
           tabBarLabel: "Explore",
           tabBarIcon: ({ size, color }) => (
-            <Ionicons name="search" size={size} color={color} />
+            <Ionicons name="search" size={24} color={color} />
           ),
         }}
       />
 
+      <Tabs.Screen
+        name="createPodcast"
+        options={{
+          tabBarLabel: "Create",
+
+          headerShown: false,
+          tabBarIcon: ({ size, color }) => (
+            <Entypo name="mic" size={24} color={color} />
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="mypodcast"
+        options={{
+          tabBarLabel: "My Podcast",
+
+          headerShown: false,
+          tabBarIcon: ({ size, color }) => (
+            <MaterialCommunityIcons name="podcast" size={24} color={color} />
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="mysubscription"
+        options={{
+          tabBarLabel: "My Subscription",
+
+          headerShown: false,
+          tabBarIcon: ({ size, color }) => (
+            <MaterialIcons name="subscriptions" size={24} color={color} />
+          ),
+        }}
+      />
       <Tabs.Screen
         name="profile"
         options={{
@@ -29,7 +66,7 @@ const Layout = () => {
 
           headerShown: false,
           tabBarIcon: ({ size, color }) => (
-            <Ionicons name="person-circle-outline" size={size} color={color} />
+            <Ionicons name="person-circle-outline" size={24} color={color} />
           ),
         }}
       />
