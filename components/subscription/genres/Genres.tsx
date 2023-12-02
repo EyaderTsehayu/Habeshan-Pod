3a2fa58ce1f12269437082f187925a6ad97035ff
Eyader Tsehayu <eyaderts@gmail.com>
2023-12-02 22:04:21 +0300

    Ui for horizontal genres

--- components/subscription/genres/Genres.tsx
+++ components/subscription/genres/Genres.tsx
@@ -1,37 +1,63 @@
 import { View, Text, FlatList, TouchableOpacity } from "react-native";
 import React from "react";
+import { useRouter } from "expo-router";
+import genreStyles from "./genres.styles";
+import { MaterialCommunityIcons } from "@expo/vector-icons";
+import { AntDesign } from "@expo/vector-icons";
+import Colors from "@/constants/Colors";
 
-// const TabButton = ({ name, activeTab, onHandleSearchType }) => (
-//   <TouchableOpacity
-//     style={styles.btn(name, activeTab)}
-//     onPress={onHandleSearchType}
-//   >
-//     <Text style={styles.btnText(name, activeTab)}>{name}</Text>
-//   </TouchableOpacity>
-// );
-const Genres = () => {
-  const tabs = ["Sport", "Technology", "Business", "Self Development"];
+interface Genre {
+  genre: string;
+  id: string;
+  icon: string;
+  uri: string;
+}
+
+const Genres: React.FC = () => {
+  const tabs: Genre[] = [
+    { genre: "Technology", id: "1", icon: "laptop", uri: "/(tabs)/" },
+    { genre: "Business", id: "2", icon: "briefcase", uri: "/(tabs)/" },
+    { genre: "Sport", id: "3", icon: "basketball", uri: "/(tabs)/" },
+    { genre: "Self Development", id: "4", icon: "book-open", uri: "/(tabs)/" },
+    { genre: "Entertainment", id: "5", icon: "popcorn", uri: "/(tabs)/" },
+  ];
+
+  const router = useRouter();
 
   return (
-    <View>
-      <Text>Genres</Text>
+    <View style={genreStyles.container}>
+      <View style={genreStyles.headerContainer}>
+        <Text style={genreStyles.headerText}>Genres</Text>
+
+        <TouchableOpacity style={genreStyles.showAllBtn}>
+          <AntDesign name="arrowright" size={30} color={Colors.headerText} />
+        </TouchableOpacity>
+      </View>
+      <FlatList
+        data={tabs}
+        renderItem={({ item }) => (
+          <TouchableOpacity
+            style={genreStyles.btn}
+            onPress={() => router.push(item.uri)}
+          >
+            <View style={{ display: "flex", gap: 6, alignItems: "center" }}>
+              <MaterialCommunityIcons
+                name={item.icon}
+                size={42}
+                color={Colors.lightNavy}
+                style={genreStyles.icon}
+              />
+              <Text style={genreStyles.btnText}>{item.genre}</Text>
+              <Text style={genreStyles.btnSubText}>14 podcasts</Text>
+            </View>
+          </TouchableOpacity>
+        )}
+        horizontal
+        showsHorizontalScrollIndicator={false}
+        keyExtractor={(item) => item.id}
+        contentContainerStyle={{ columnGap: 6 }}
+      />
     </View>
-    // <View style={styles.container}>
-    //   <FlatList
-    //     data={tabs}
-    //     renderItem={({ item }) => (
-    //       <TabButton
-    //         name={item}
-    //         activeTab={activeTab}
-    //         onHandleSearchType={() => setActiveTab(item)}
-    //       />
-    //     )}
-    //     horizontal
-    //     showsHorizontalScrollIndicator={false}
-    //     keyExtractor={(item) => item}
-    //     contentContainerStyle={{ columnGap: SIZES.small / 2 }}
-    //   />
-    // </View>
   );
 };
 
--- components/subscription/genres/genres.styles.js
+++ components/subscription/genres/genres.styles.js
@@ -1,12 +1,54 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import Colors from "@/constants/Colors";
+import { StyleSheet } from "react-native";
 
-const genres.styles = () => {
-  return (
-    <View>
-      <Text>genres.styles</Text>
-    </View>
-  )
-}
+const genreStyles = StyleSheet.create({
+  container: {
+    marginTop: 6,
 
-export default genres.styles
+    paddingBottom: 12,
+    paddingLeft: 6,
+  },
+  btn: {
+    display: "flex",
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "center",
+    columnGap: 12,
+    paddingVertical: 12,
+    paddingHorizontal: 24,
+    backgroundColor: "#F3F4F8",
+    borderRadius: 10,
+    marginHorizontal: 2,
+    shadowColor: Colors.lightBg,
+  },
+  btnText: {
+    fontFamily: "dm-b",
+    fontSize: 16,
+    color: Colors.headerText,
+  },
+  headerContainer: {
+    display: "flex",
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "space-between",
+  },
+  btnSubText: {
+    fontFamily: "dm-sb",
+    fontSize: 12,
+    color: Colors.headerText,
+  },
+  headerText: {
+    marginLeft: 20,
+    fontFamily: "dm-b",
+    fontSize: 28,
+    color: Colors.headerText,
+    paddingBottom: 12,
+  },
+  showAllBtn: {
+    paddingBottom: 12,
+    marginRight: 24,
+  },
+  icon: {},
+});
+
+export default genreStyles;
