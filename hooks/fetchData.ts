493fd7a1fa4f4d37ce137fa504b1dc5179a87cd7
Eyader Tsehayu <eyaderts@gmail.com>
2023-12-20 23:32:41 +0300

    add data fetching and filtering custom hook

---
+++ hooks/fetchData.ts
@@ -0,0 +1,62 @@
+import { useState, useEffect } from "react";
+import { storage, db } from "../firebaseConfig";
+import {
+  collection,
+  QuerySnapshot,
+  DocumentData,
+  getDocs,
+} from "firebase/firestore";
+import { useUser } from "@clerk/clerk-expo";
+interface Podcast {
+  id: string;
+  userId: string;
+  firstName: string;
+  lastName: string;
+  title: string;
+  audioUrl: string;
+  coverImageUrl: string;
+  description: string;
+  episode: number;
+}
+
+const useFirebaseData = () => {
+  const [myPodData, setMyPodData] = useState<Podcast[]>([]);
+  const [podcasts, setPodcasts] = useState<Podcast[]>([]);
+  const { user } = useUser();
+
+  useEffect(() => {
+    const fetchPodcasts = async () => {
+      try {
+        const podcastCollection = collection(db, "podcasts");
+        const querySnapshot: QuerySnapshot<DocumentData> = await getDocs(
+          podcastCollection
+        );
+        console.log(querySnapshot);
+
+        const fetchedPodcasts: Podcast[] = [];
+
+        querySnapshot.forEach((doc) => {
+          const podcastData = doc.data() as Podcast;
+          fetchedPodcasts.push({ ...podcastData, id: doc.id });
+        });
+
+        setPodcasts(fetchedPodcasts);
+      } catch (error) {
+        console.error("Error fetching podcasts: ", error);
+      }
+    };
+
+    fetchPodcasts();
+  }, []);
+
+  useEffect(() => {
+    const filteredMyPodData = podcasts.filter(
+      (item) => item.userId === user?.id
+    );
+    setMyPodData(filteredMyPodData);
+  }, [podcasts]);
+
+  return { podcasts, myPodData };
+};
+
+export default useFirebaseData;
