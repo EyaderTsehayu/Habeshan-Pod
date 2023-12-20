7a9bbf03b88dee8650eece024020f1cbd513f68a
Eyader Tsehayu <eyaderts@gmail.com>
2023-12-20 23:35:17 +0300

    change mock data by real and send data to player

--- components/mypod/podList/MyPodList.tsx
+++ components/mypod/podList/MyPodList.tsx
@@ -3,16 +3,7 @@ import myPodListstyles from "./myPodList.styles";
 import React, { useEffect, useState } from "react";
 import MyPodCard from "@/components/common/cards/myPod/MyPodCard";
 import { Image } from "react-native";
-import {
-  DocumentData,
-  QuerySnapshot,
-  collection,
-  getDocs,
-  query,
-  where,
-} from "firebase/firestore";
-import { db } from "@/firebaseConfig";
-import { useUser } from "@clerk/clerk-expo";
+import useFirebaseData from "@/hooks/fetchData";
 
 interface Podcast {
   id: string;
@@ -26,46 +17,22 @@ interface Podcast {
   episode: number;
 }
 const MyPodList = () => {
-  const { user } = useUser();
+  const { myPodData } = useFirebaseData();
 
-  const [podcasts, setPodcasts] = useState<Podcast[]>([]);
-
-  useEffect(() => {
-    const fetchPodcasts = async () => {
-      try {
-        const q = query(
-          collection(db, "podcasts"),
-          where("userId", "==", user?.id)
-        );
-
-        const querySnapshot: QuerySnapshot<DocumentData> = await getDocs(q);
-
-        const fetchedPodcasts: Podcast[] = [];
-
-        querySnapshot.forEach((doc) => {
-          const podcastData = doc.data() as Podcast;
-          fetchedPodcasts.push({ ...podcastData, id: doc.id });
-        });
-
-        setPodcasts(fetchedPodcasts);
-      } catch (error) {
-        console.error("Error fetching podcasts: ", error);
-        // Handle error state or display a message to the user
-      }
-    };
-
-    fetchPodcasts();
-  }, []);
-
-  if (podcasts.length > 0) {
+  if (myPodData.length > 0) {
     return (
       <View style={myPodListstyles.container}>
         <View style={myPodListstyles.headerContainer}>
           <Text style={myPodListstyles.headerTitle}>My Tracks</Text>
         </View>
         <View style={myPodListstyles.cardContainer}>
-          {podcasts.map((item) => (
-            <MyPodCard key={item.id} item={item} />
+          {myPodData.map((item, index) => (
+            <MyPodCard
+              key={item.id}
+              item={item}
+              index={index}
+              podcasts={myPodData}
+            />
           ))}
         </View>
       </View>
